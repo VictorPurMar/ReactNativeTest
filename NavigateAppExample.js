3459080da1ee9ff81e6e9c36eab08efea3cb7b13
Victor Purcallas <victor.purcallas@mycujoo.tv>
2020-05-29 13:49:35 +0200

    Playing with files, and getting content

--- NavigateAppExample.js
+++ NavigateAppExample.js
@@ -3,7 +3,21 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+// const MyApp = MultiNavigator({
+//   Home: {screen: HomeScreen},
+//   Details: {screen: DetailsScreen}
+// });
+
+// const MultiNavigator = (routeConfigs) => {
+//   const modalNavigatorRoutes = {};
+//   Object.keys(routeConfigs).forEach(routeName => {
+//     const InnerNavigator = StackNavigator(routeConfigs);
+//     modalNavigatorRoutes[routeName+'Modal'] = { screen: InnerNavigator };
+//   });
+//   return StackNavigator(modalNavigatorRoutes, {mode: 'modal'});
+// };
+
+export function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -15,7 +29,7 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen({ navigation }) {
+export function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
@@ -29,17 +43,4 @@ function DetailsScreen({ navigation }) {
   );
 }
 
-const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
